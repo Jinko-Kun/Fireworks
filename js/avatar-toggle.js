3243b462b96e10cb3eee50014697a89e288d03c3
Yukishi <jgomez19996@gmail.com>
2025-03-30 11:19:33 -0600

    more more

--- js/avatar-toggle.js
+++ js/avatar-toggle.js
@@ -1,15 +1,15 @@
 const image = document.getElementById('avatar');
 
 image.addEventListener('click', function () {
-  if (image.src.match('js/material/Untitled(40).png')) {
-    image.src = 'js/material/Untitled(41).png';
-  } else if (image.src.match('js/material/Untitled(41).png')) {
-    image.src = 'js/material/Untitled(43).png';
-  } else if (image.src.match('js/material/Untitled(43).png')) {
-    image.src = 'js/material/Untitled(44).png';
-  } else if (image.src.match('js/material/Untitled(44).png')) {
-    image.src = 'js/material/Untitled(45).png';
+  if (image.src.match('material/Untitled(40).png')) {
+    image.src = 'material/Untitled(41).png';
+  } else if (image.src.match('material/Untitled(41).png')) {
+    image.src = 'material/Untitled(43).png';
+  } else if (image.src.match('material/Untitled(43).png')) {
+    image.src = 'material/Untitled(44).png';
+  } else if (image.src.match('material/Untitled(44).png')) {
+    image.src = 'material/Untitled(45).png';
   } else {
-    image.src = 'js/material/Untitled(40).png';
+    image.src = 'material/Untitled(40).png';
   }
 });
